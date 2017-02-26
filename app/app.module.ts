a382b9292ccc2a85eac3d1be233b7130dc671261
Daniel Arnolf <daniela@spiralsolutions.co.il>
2017-02-26 16:49:00 +0200

    Initial Commit

---
+++ app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [
+        EventsAppComponent,
+        EventListComponent,
+        EventThumbnailComponent
+    ],
+    bootstrap:  [EventsAppComponent]
+})
+
+export class AppModule  {
+
+
+}
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+    <h1>Upcoming Angular 2 Events</h1>
+
+    <event-thumbnail [event]="event1" (eventClick)="handleEventCLicked($event)"></event-thumbnail>
+
+
+    </div>`
+})
+
+export class EventListComponent  { 
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+          address: '1057 DT',
+          city: 'London',
+          country: 'England'
+        }
+    }
+    handleEventCLicked(data){
+        console.log('received: ', data);
+    }
+}
