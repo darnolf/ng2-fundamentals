a77d1c42dbc159be96a2cc21bb7f3ebadc2fae28
darnolf <arnolf10@gmail.com>
2017-02-26 22:21:49 +0200

    Change to lite-server generic

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -16,17 +16,17 @@ export class EventListComponent  {
     event1 = {
         id: 1,
         name: 'Angular Connect',
-        date: '9/26/2036',
-        time: '10:00 am',
-        price: 599.99,
+            date: '9/26/2036',
+        time: '10:00 am', 
+                price: 599.99,
         imageUrl: '/app/assets/images/angularconnect-shield.png',
         location: {
           address: '1057 DT',
           city: 'London',
-          country: 'England'
+          country: 'Argentina'
         }
     }
     handleEventCLicked(data){
-        console.log('received: ', data);
+        console.log('received changed: ', data);
     }
 }
