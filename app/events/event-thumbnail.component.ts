f8b4b41516329b405bc7d2d163477b03f30b4e49
Daniel Arnolf <daniela@spiralsolutions.co.il>
2017-02-27 08:32:05 +0200

    Creating and comunicating between Components

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -18,8 +18,5 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventThumbnailComponent  { 
     @Input() event: any
-    logFoo() {
-        console.log('foo')
-    }
 
  }
